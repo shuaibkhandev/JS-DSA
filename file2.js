400c8b1496fcfaf74bf135eb2a72040bdb3d14a1
shuaibkhan_unitedsol <skhan.csit@gmail.com>
2025-05-12 10:59:50 +0500

    array traversing and accessing

---
+++ file2.js
@@ -0,0 +1,20 @@
+// What is Array Traversing?
+// Array Traversing means visiting each element of the array one by one, usually to read, update, or perform some operation on each element.
+
+
+// const data = [10,20,30,40,50,60,70,80,90];
+
+// Traversing using for loop
+// for(let i=0; i< data.length; i++){
+//     const demo = document.getElementById("demo");
+//     demo.innerHTML += `${i} index of array is ${data[i]} <br/>`;  //  Accessing Each Element
+// }
+
+
+
+
+//  What is Array Accessing?
+// Array Accessing means retrieving a specific element in the array using its index.
+
+const data = [10,20,30,40,50,60,70,80,90];
+document.getElementById('demo').innerHTML = `${data[2]}` //Accessing the third Element -> 30
