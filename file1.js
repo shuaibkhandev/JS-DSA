200cd38916d89117525c442d317295e4b0b9fecd
shuaibkhan_unitedsol <shuaib.khan@unitedsol.net>
2025-05-12 10:26:09 +0500

    intro DSA

---
+++ file1.js
@@ -0,0 +1,13 @@
+// What is a Data Struture ?
+// A way to organize and store data in a computer so that it can be accessed and modified efficiently.
+
+// Example:
+// Unorganized Data: [7, 2, 4, 9, 0, 6, 3, 1, 5, 8] 😣
+// Organized Data (Sorted): [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 😍
+
+
+// What is sn Algorithm?
+// A set of well-defined instructions or steps to solve a problem or perform a task (like arranging data).
+
+// Example:
+// The process used to sort the unorganized array into an ordered one is an Algorithm (e.g., Bubble Sort, Quick Sort, etc.).
