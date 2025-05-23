4a1ef93a923388578d2f95322f916598b0d7aa2f
shuaibkhan_unitedsol <skhan.csit@gmail.com>
2025-05-23 10:30:12 +0500

    sort array using bubble sort

--- file6.js
+++ file6.js
@@ -1,1 +1,21 @@
 // Sort Array 
+
+const data = [42, 7, 13, 99];
+let sum = 0;
+for(let i = 0; i<data.length; i++){
+
+
+
+    for(let j =0; j<data.length; j++){
+      if(data[i] > data[i+1]){
+        let tempNum = data[i];
+        data[i] = data[i+1];
+        data[i+1] = tempNum;
+    }
+        
+    }
+ 
+}
+console.log(data);
+
+
